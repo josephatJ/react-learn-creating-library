dac7d707df2d7674bf06f4ea946af92606b6afbc
josephatJ <josephatjulius24@gmail.com>
2019-11-19 11:44:10 +0300

    set up environment for publishing a react component to npm

---
+++ dist/Testing.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Testing.css";
+
+function Testing() {
+  return React.createElement("div", null, React.createElement("h4", {
+    className: "testing"
+  }, "Testing the library"));
+}
+
+export default Testing;
---
+++ src/components/Testing.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Testing.css";
+
+function Testing() {
+  return (
+    <div>
+      <h4 className="testing">Testing the library</h4>
+    </div>
+  );
+}
+
+export default Testing;
